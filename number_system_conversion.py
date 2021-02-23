54379247ee2edd4d3d417484d8dbcd3840369fc5
nutshelf <hi.how.are.you.pluto@gmail.com>
2021-02-23 16:06:20 +0300

    Программа преобразования систем счисления выделена в отдельный файл

--- number_system_conversion.py
+++ number_system_conversion.py
@@ -1,9 +1,12 @@
 x = 0
 while True:
     x = int(input("Введи число в десятичной системе счисления: "))
-    base = int(input("Введи основание системы счисления в десятичной системе счисления: "))
-    number_string = "=" + str(bin(x))
+    base = int(input('Введи основание системы счисления в десятичной системе счисления: '))
+    number_string = ""
+    # обрамления каждого разряда скобочками для систем счисления с основанием, большим 10
+    br1 = "(" * (base > 10)
+    br2 = ")" * (base > 10)
     while x > 0:
-        number_string = str(x % base) + number_string
+        number_string = br1 + str(x % base) + br2 + number_string
         x = x // base
     print(number_string)
