e87c8c48d7e72253c73df7d9d1ddfcd3f1b541b4
nutshelf <hi.how.are.you.pluto@gmail.com>
2021-02-26 22:22:35 +0300

    Добавлена игра в крестики нолики tictactoeing.py Начата разработка игры только.

--- tictactoeing.py
+++ tictactoeing.py
@@ -3,16 +3,21 @@ user_symbol = "O"
 ai_symbol = "X"
 current_user = [user_symbol, ai_symbol]  # список для определения, чей ход
 current_user_pointer = 0  # индекс для списка
+game_is_on = True
 
 # ход игры
-while (available_cells(field_state) and not someone_wins(field_state)):
+while game_is_on:
     # делают ходы
+
+    # завершение хода
     current_user_pointer = (current_user_pointer + 1) % 2
+    there_is_winner = someone_wins_detection(field_state)
+    game_is_on = available_cells(field_state) and not there_is_winner
+
 # игра окончена
-winner = someone_wins(field_state)
-if winner == user_symbol:
+if there_is_winner == user_symbol:
     print("you wins!")
-elif winner == ai_symbol:
+elif there_is_winner == ai_symbol:
     print("AI wins!")
 else:
     print("some error ocures! winning detection error. Winner is ", winner, ". Exiting")
