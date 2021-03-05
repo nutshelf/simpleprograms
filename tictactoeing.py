3706c16af0ed3f8a21e3006f8d057ff0ef3aca63
nutshelf <nu6optbi>
2021-03-05 16:31:50 +0300

    Тесты: алгоритм хромает. Моежт выиграть. но не ходит в эту клетку, т.к. вероятность где-то выше.

--- tictactoeing.py
+++ tictactoeing.py
@@ -1,6 +1,11 @@
 def draw_field(field):
-    """ ПР: Замещение цифр 0-9 на пробел и вывод поля  """
-    for i in range(10):
+    """ ПР: Замещение цифр 1-9 на пробел и вывод поля. При обращении к строке как к итерируемому типу данных учтено, что
+    номер отличается на единицу, то есть номера для обращения:
+    012
+    345
+    678
+    """
+    for i in range(1, 10):
         field = field.replace(str(i), " ")
     print(field[0] + "|" + field[1] + "|" + field[2])
     print("-+-+-")
@@ -11,11 +16,12 @@ def draw_field(field):
 
 
 def get_available_cells(field):
-    """ Ф: Результат - список из номеров "пустых" ячеек. """
+    """ Ф: Результат - список из номеров "пустых" ячеек. Номер [0;8] для удобства обращения к элементам массива
+    """
     list_of_cells = []
     for cell in field:
         if cell in "123456789":
-            list_of_cells.append(cell)
+            list_of_cells.append(int(cell)-1)
     return list_of_cells
 
 
@@ -51,7 +57,7 @@ def suppose_turn(field, cu_pointer, gamers_symbol):
         user_wins, ai_wins, draw = 0, 0, 0
         for cell in available_cell:
             ai_turn_result = suppose_turn(
-                field.replace(str(cell), gamers_symbol[cu_pointer]),
+                field.replace(str(cell + 1), gamers_symbol[cu_pointer]),
                 cu_pointer,
                 gamers_symbol)
             user_wins += ai_turn_result[0]
@@ -60,15 +66,15 @@ def suppose_turn(field, cu_pointer, gamers_symbol):
     return [user_wins, ai_wins, draw]
 
 
-def main():
+def main1():
     """ Game field(board)
     123
     456
     789
     """
     field = "123456789"
-    user_symbol = "O"
-    ai_symbol = "X"
+    user_symbol = "X"
+    ai_symbol = "O"
     symbols = [user_symbol, ai_symbol]
     cu_pointer = 0  # current user pointer - индекс игрока для совершения хода. 0 - ходит игрок, 1 - ходит компьютер
     game_is_on = True
@@ -79,9 +85,9 @@ def main():
         draw_field(field)
         if cu_pointer == 0:  # ход живого юзера
             while True:
-                user_turn_cell_num = int(input("Ваш ход (введите номер клетки 1-9):"))
+                user_turn_cell_num = -1 + int(input("Ваш ход (введите номер клетки 1-9):"))
                 if field[user_turn_cell_num] in "123456789":
-                    field = field.replace(str(user_turn_cell_num), user_symbol)
+                    field = field.replace(str(user_turn_cell_num + 1), user_symbol)
                     break
                 else:
                     print("ой, не туда! там уже кто-то побывал. еще разок")
@@ -93,23 +99,24 @@ def main():
                 max_ratio_cell_index = 0
                 max_ratio_cell_results = 0
                 for cell in available_cells:
-                    ai_turn_result = suppose_turn(field.replace(str(cell), ai_symbol), 0, symbols)
+                    ai_turn_result = suppose_turn(field.replace(str(cell + 1), ai_symbol), 0, symbols)
                     # главный алгоритм для определения клетки для хода - процент побед ИИ от всех возможных исходов
                     available_cell_win_ratio.append(ai_turn_result[1] / sum(ai_turn_result))
                     if available_cell_win_ratio[-1] > max_ratio:
                         max_ratio = available_cell_win_ratio[-1]
                         max_ratio_cell_index = cell
                         max_ratio_cell_results = ai_turn_result # удалить потом можно эту и следующую строку
-                print("Из доступных клеток самая выгодная №", max_ratio_cell_index,
-                      ". Поб:пр:нич =", max_ratio_cell_results,
-                      "максимальная вероятность выигрыша по всем достпуным клеткам", max_ratio)
+                print("ИИ ходит. Из доступных клеток самая выгодная №", max_ratio_cell_index + 1,
+                      ". Поб:пр:нич = ", max_ratio_cell_results,
+                      f". В ней максимальна вероятность выигрыша по всем клеткам {max_ratio:0.3f}")
             else:  # len(available_cells) = 1
                 max_ratio_cell_index = available_cells
-            field = field.replace(max_ratio_cell_index, ai_symbol)
+            field = field.replace(str(max_ratio_cell_index + 1), ai_symbol)
         cu_pointer = (cu_pointer + 1) % 2
         winner_is = who_wins(field, user_symbol, ai_symbol)
         game_is_on = get_available_cells(field) and not winner_is
     # игра окончена
+    draw_field(field)
     if winner_is == user_symbol:
         print("you wins!")
     elif winner_is == ai_symbol:
@@ -122,4 +129,4 @@ def main():
               "), но игра закончена и никто не выиграл. Чудеса! еррор конечно")
 
 
-main()
+main1()
