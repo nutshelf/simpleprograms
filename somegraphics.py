4a6bbb1314ba010fbc9e4c78ebc873617f410a2d
nutshelf <hi.how.are.you.pluto@gmail.com>
2021-03-15 22:42:16 +0300

    начал рисовать фрактал

--- somegraphics.py
+++ somegraphics.py
@@ -1,16 +1,14 @@
 import graphics as gr
 
 window = gr.GraphWin("some title", width=500, height=500)
-A = 100, 100
-B = 400, 400
 
 
 def drawLine(p1, p2, depth=20):
     """
     рисуем фрактальную линию с поворотом вправо на одной трети
     """
-
-    pass
+    gr.Line(gr.Point(*p1), gr.Point(*p2)).draw(window)
+    return
 
 
 # gr.Line(gr.Point(*A), gr.Point(B[0], A[1])).draw(window)
@@ -18,5 +16,5 @@ def drawLine(p1, p2, depth=20):
 # gr.Line(gr.Point(*B), gr.Point(A[0], B[1])).draw(window)
 # gr.Line(gr.Point(A[0], B[1]), gr.Point(*A)).draw(window)
 
-drawLine(A, B, 20)
+drawLine((100, 100), (400, 100), 20)
 window.wait_window()
