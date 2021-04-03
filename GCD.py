5da6cdd6993935570e7cd5de8dac29cc4644a2e0
nutshelf <hi.how.are.you.pluto@gmail.com>
2021-04-03 14:02:45 +0300

    GCD допилил

--- GCD.py
+++ GCD.py
@@ -2,9 +2,8 @@
 # Greatest Common Divider GCD
 # Euclidean Recursive algorithm & with FOR cycle
 
-import time
 
-def gcd_incycle(a, b):
+def gcd_incycle(a: int, b: int):
     op = 0
     while True:
         op += 1
@@ -18,27 +17,28 @@ def gcd_incycle(a, b):
                 return a
 
 
-def gcd_recursive(a, b, op=1):
+def gcd_recursive(a: int, b: int, depth=1):
     """
-    In mind a > b, otherwise one recursive call will be done
+    In mind a > b, otherwise one additional recursive call for args changing will be done
     """
     if b == 0:
-        print("количество вызовов рекурсивной функции =", op)
+        print("recursive calls =", depth)
         return a
-    return gcd_recursive(b, a % b, op + 1)
+    return gcd_recursive(b, a % b, depth + 1)
 
 
 def main1():
-    a = 49238281822
-    b = 43347347727
+    a = 18
+    b = 15
 
-    start_time = time.time()
-    print("gcd_incycle =", gcd_incycle(a, b))
-    print("t=", round(time.time()-start_time, 3))
-
-    start_time = time.time()
-    print("gcd_recursive =", gcd_recursive(a, b))
-    print("t=", time.time() - start_time)
+    gcd1 = gcd_incycle(a, b)
+    gcd2 = gcd_recursive(a, b)
+    if gcd1 == gcd2:
+        print("Results in both methods are EQUAL. GCD is", gcd1)
+    else:
+        print("Results in two methods are not equal!\n",
+              "In cycle method:", gcd1, "\n",
+              "Recursive method:", gcd2)
 
 
 main1()
