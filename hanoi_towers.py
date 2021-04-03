7a38058266e3e92edfcde452ff1b4e05914a488f
nutshelf <hi.how.are.you.pluto@gmail.com>
2021-04-04 00:41:10 +0300

    надо намутить хайнойских башень чуть-чуть

--- hanoi_towers.py
+++ hanoi_towers.py
@@ -1,1 +1,18 @@
-def move_disk ()
+def move_disk(n, fr, th, to):
+    """
+    Move disk N from FR through TH to TO
+    """
+    if n == 1:
+
+    else:
+
+    pass
+
+
+def main1():
+    n = 1
+
+    pass
+
+
+main1()
