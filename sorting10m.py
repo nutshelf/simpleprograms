4f1c989c701b2e02bc0a9149a9a32c53613d169e
nutshelf <nu6optbi>
2021-04-16 13:18:41 +0300

    combination.py: Need to write binary search in sorted list with detection index of one equal element or boundary to insertion

--- sorting10m.py
+++ sorting10m.py
@@ -6,14 +6,16 @@
  Считаем, что повторы не надо.
 """
 
-
+import time
+from math import sqrt as sqrt
 from random import random as rnd
 
 
 def is_prime(x: int):
-    if x % 2 == 0 and x != 2:
-        return False
-    for i in range(3, int(x**0.5)+1, 2):
+    if x % 2 == 0:
+        if x != 2:
+            return False
+    for i in range(3, int(sqrt(x))+1, 2):
         if x % i == 0:
             return False
     return True
@@ -25,37 +27,70 @@ def add(a: list, x: int):
             a.insert(i + 1, x)
             return
     a.insert(0, x)
+    # a.append(x) # simple appending
     return
 
 
-def print_a(a: list):
+def print_a(a: list, num_in_row):
     print(a[0], end=" ")
     i = 1
     while i < len(a):
         print(a[i], end=" ")
         i += 1
-        if i % 10 == 0:
+        if i % num_in_row == 0:
             print()
     return
 
 
-def main():
+def item_in_list_binary_search(a: list, x, lb=None, ub=None):
+    if lb == None:
+        lb = 0
+    if ub == None:
+        ub = len(a)
+    mid = (lb + ub) / 2
+    #desc order list
+    if a[0] < a[-1]:
+        if a[mid] < x:
+
+    # old slowest algorithm
+    # for i in range(len(a)):
+    #     if a[i] == x:
+    #         return True
+    # return False
+
+
+def main(num=1*100*1):
     a = []
-    max = 0
-    for i in range(1000000):
-        x = int(rnd() * 10 ** (rnd() * 5)) + 2
-        if x > max:
-           max = x
-        #print(x, end=" ") #output all numbers
-        if not (x in a) and is_prime(x):
+    start_time = time.time()
+    time_prime_total = 0.0
+    time_find_dupls_total = 0.0
+    time_add_list_total = 0.0
+    for i in range(num):
+        x = int(rnd() * (10 ** (rnd() * 5)) + 2)
+
+        time1 = time.time()
+        is_prime_bool = is_prime(x)
+        time_prime_total += time.time() - time1
+
+        time2 =  time.time()
+        x_in_a_bool = item_in_list_binary_search(a, x)
+        time_find_dupls_total += time.time() - time2
+
+        if not x_in_a_bool and is_prime_bool:
+            time3 = time.time()
             add(a, x)
-    print("max num at all:", max)
-    if a:
-        #print("All prime numbers in ascending order:")
-        #print_a(a)
-        print("Quantity of numbers", len(a))
-    else:
-        print("There were no prime numbers")
+            time_add_list_total += time.time() - time3
+
+    print(f"Num of source numbers: {num:_}")
+    print(f"Num of prime numbers: {len(a):_}")
+    print(f"Total time FOR x {num}: {time.time() - start_time:.3}s")
+    print(f"Total time for prime checking: {time_prime_total:.3}s")
+    print(f"Total time for finding duplicates in a: {time_find_dupls_total:.3}s")
+    print(f"Total time for adding results to list: {time_add_list_total:.3}s")
+    # 1m elements => 32s
+
+    print_a(a, 30)
+    print()
     return
 
 
