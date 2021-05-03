9884dacd2a28d85c60ee8c026a9265445282afce
nutshelf <hi.how.are.you.pluto@gmail.com>
2021-05-03 22:42:31 +0300

    sorting10m.py: Last version with timing and redundant functions (exact binary search results: 'exact'/'low'/'range'/'upper' pointers)

--- sorting10m.py
+++ sorting10m.py
@@ -1,9 +1,12 @@
 """
-Дан массив размер 10кк элементов типа int, все элементы массива  > 0, расставлены в произвольном порядке, значения
-элементов могут повторятся, значения произвольны. Найти все простые числа. Записать их по порядку в другой массив, можно
- не упорядочивать.
+There are 10'000'000 int numbers, all greater then null. Array of this numbers is not sorted. Numbers can repeat
+in array. Task is to find prime numbers and put then in ascending order in new array. Prime numbers in new array
+must not repeat.
 
- Считаем, что повторы не надо записывать.
+Исходный текст (отличающийся по условиям), из которого родился алгоритм ниже:
+"Дан массив размер 10кк элементов типа int, все элементы массива  > 0, расставлены в произвольном порядке, значения
+элементов могут повторятся, значения произвольны. Найти все простые числа. Записать их по порядку в другой массив, можно
+ не упорядочивать."
 """
 
 import time
@@ -12,6 +15,8 @@ from random import random as rnd
 
 
 def is_prime(x: int):
+    if x == 1:
+        return False
     if x % 2 == 0:
         if x != 2:
             return False
@@ -47,10 +52,10 @@ def print_fw(list1, row_width, rest_width=None):
     return rest_width
 
 
-def item_in_list_binary_search(a: list, x):
+def binary_search(a: list, x):
     """
-    Function for search in sorted list of elements in increasing order.
-    Output is [string, index to insert x]. Sorted order will be saved
+    Function for search in sorted in increasing order list of elements.
+    Output is [True/False, int]. Last one is index where to insert x. Sorted order will be saved after inserting.
     ['exact', i]        if a[i] == x
     ['range', i]        if a[i-1] < x < a[i], inserting at a[i]
     ['lower', 0]        if x < a[0]
@@ -58,7 +63,7 @@ def item_in_list_binary_search(a: list, x):
     """
     lb = 0
     ub = len(a) - 1
-    if a == []:  # no elements in list
+    if not a:  # no elements in list
         return ['upper', 0]
     if ub == 0:  # one element in list
         if x < a[0]:
@@ -75,6 +80,7 @@ def item_in_list_binary_search(a: list, x):
             ub = mid
         else:
             lb = mid
+
     if a[lb] < x < a[ub]:
         output = ['range', ub]
     elif x < a[lb]:
@@ -86,39 +92,11 @@ def item_in_list_binary_search(a: list, x):
     elif x == a[ub]:
         output = ['exact', ub]
     else:
-        raise ValueError('Error in func item_in_list_binary_search')
+        raise ValueError('Error in func binary_search')
     return output
 
 
-# def item_in_list_binary_search_r(a: list, x, lb=None, ub=None):
-#
-#     if not a:
-#         return ['upper', 0]
-#     if (lb is None) or (ub is None):  # default lower & upper bounds
-#         lb = 0
-#         ub = len(a) - 1
-#     if (lb + 1 == ub) or (lb == ub):
-#         if x < a[lb]:
-#             return ['low', 0]
-#         if x > a[ub]:
-#             return ['upper', len(a)]
-#         if x == a[lb]:
-#             return ['exact', lb]
-#         if a[lb] < x < a[ub]:
-#             return ['range', ub]
-#         if x == a[ub]:
-#             return ['exact', ub]
-#
-#     # one or more elements between lb and ub
-#     mid = int((lb + ub) / 2)
-#     if x < a[mid]:
-#         res = item_in_list_binary_search(a, x, lb, mid)
-#     else:
-#         res = item_in_list_binary_search(a, x, mid, ub)
-#     return res
-
-
-def main(num=10 * 10 * 1):
+def main(num=10 * 1000 * 1000):
     a = []
     start_time = time.time()
     time_prime_total = 0.0
@@ -129,55 +107,56 @@ def main(num=10 * 10 * 1):
     range_total_count = 0
     low_total_count = 0
     upper_total_count = 0
+    low_numbers = []
+    upper_numbers = []
 
     for i in range(num):
-        x = int(rnd() * 10000 + 2)
+        x = int(rnd() * 1000000000 + 1)  # [1, 1'000'000'000]
 
         time1 = time.time()
         is_prime_bool = is_prime(x)
         time_prime_total += time.time() - time1
 
         time2 = time.time()
-        x_in_a_result = item_in_list_binary_search(a, x)
+        x_in_a_result = binary_search(a, x)
         x_in_a_bool = x_in_a_result[0] == 'exact'
         time_find_duplicates_total += time.time() - time2
+
         if x_in_a_result[0] == 'exact':
             exact_total_count += 1
-        elif: x_in_a_result[0] == 'exact'
-            range_total_count +=
-        elif x < a[lb]:
-        output = ['low', 0]
-        elif x > a[ub]:
-        output = ['upper', len(a)]
-        elif x == a[lb]:
-        output = ['exact'
-
-
 
         time3 = time.time()
         if not x_in_a_bool and is_prime_bool:
+            if x_in_a_result[0] == 'range':
+                range_total_count += 1
+            elif x_in_a_result[0] == 'low':
+                low_total_count += 1
+                low_numbers.append(x)
+            elif x_in_a_result[0] == 'upper':
+                upper_total_count += 1
+                upper_numbers.append(x)
             add(a, x, x_in_a_result[1])
         time_add_list_total += time.time() - time3
 
     print(f"Num of source numbers: {num:_}")
     print(f"Num of prime numbers: {len(a):_}")
-    print(f"Total time FOR x {num:_}: {time.time() - start_time:.3}s")
+    print(f"Total time FOR x {num:_}: {time.time() - start_time:.4}s")
     print(f" time for prime checking: {time_prime_total:.3}s")
     print(f" time for finding duplicates in a: {time_find_duplicates_total:.3}s")
-
-
-
+    print(f"  binary search result: exact - {exact_total_count}")
+    print(f"  binary search result: range - {range_total_count}")
+    print(f"  binary search result: low - {low_total_count}")
+    print(f"   lower numbers:", low_numbers)
+    print(f"  binary search result: upper - {upper_total_count}")
+    print(f"   upper numbers:", upper_numbers)
     print(f" time for adding results to list: {time_add_list_total:.3}s")
-    # 10m => 55.4s
-    # 1229 p.n./ 10m n.
-    # 38.5s for finding duplicates
 
     if len(a) > 101:
-        rest_width = print_fw((a[0:50]), 150)
-        rest_width = print_fw("...", 150, rest_width)
-        print_fw(a[-50:], 150, rest_width)
+        rest_width = print_fw((a[0:50]), 100)
+        rest_width = print_fw("...", 100, rest_width)
+        print_fw(a[-50:], 100, rest_width)
     else:
-        print_fw(a, 150)
+        print_fw(a, 100)
     print()
     return
 
